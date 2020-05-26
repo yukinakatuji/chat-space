3c25f514e3ed25b718fd07e99d781f9b350d8105
yukinakatuji <sizuku1216drop@yahoo.co.jp>
2020-05-26 12:42:12 +0900

    messageコントローラー定義

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,13 @@
 class MessagesController < ApplicationController
   def index
   end
+
+  def create
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+    
+  end
 end
